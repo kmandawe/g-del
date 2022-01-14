946096b6374c7814770d1f95cda6830d0cc27812
kmandawe <kdmandawe06@gmail.com>
2022-01-14 14:38:32 +0800

    cost rules and precedence tests (service)

--- src/test/java/com/kensbunker/gdel/service/impl/CostCalculatorServiceImplTest.java
+++ src/test/java/com/kensbunker/gdel/service/impl/CostCalculatorServiceImplTest.java
@@ -10,23 +10,39 @@ import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-
 @SpringBootTest
 @ActiveProfiles(value = "test")
 class CostCalculatorServiceImplTest {
 
-    @Autowired
-    private CostCalculatorService costCalculatorService;
+  @Autowired private CostCalculatorService costCalculatorService;
 
-    @Test
-    public void testRejectCondition() {
-        GParcel parcel = new GParcel(51.0, 50, 50, 50);
-        assertEquals(CostRule.REJECT, costCalculatorService.getCostRule(parcel));
-    }
+  @Test
+  public void testRejectCondition() {
+    GParcel parcel = new GParcel(51.0, 50, 50, 50);
+    assertEquals(CostRule.REJECT, costCalculatorService.getCostRule(parcel));
+  }
 
-    @Test
-    public void testHeavyParcelCondition() {
-        GParcel parcel = new GParcel(50.0, 50, 50, 50);
-        assertEquals(CostRule.HEAVY_PARCEL, costCalculatorService.getCostRule(parcel));
-    }
+  @Test
+  public void testHeavyParcelCondition() {
+    GParcel parcel = new GParcel(50.0, 50, 50, 50);
+    assertEquals(CostRule.HEAVY_PARCEL, costCalculatorService.getCostRule(parcel));
+  }
+
+  @Test
+  public void testSmallParcelCondition() {
+    GParcel parcel = new GParcel(10.0, 10, 10, 10);
+    assertEquals(CostRule.SMALL_PARCEL, costCalculatorService.getCostRule(parcel));
+  }
+
+  @Test
+  public void testMediumParcelCondition() {
+    GParcel parcel = new GParcel(10.0, 10, 20, 10);
+    assertEquals(CostRule.MEDIUM_PARCEL, costCalculatorService.getCostRule(parcel));
+  }
+
+  @Test
+  public void testLargeParcelCondition() {
+    GParcel parcel = new GParcel(10.0, 10, 20, 20);
+    assertEquals(CostRule.LARGE_PARCEL, costCalculatorService.getCostRule(parcel));
+  }
 }
