1fc73013c361eb29e173345ff9467e47f34543de
kmandawe <kdmandawe06@yahoo.com.ph>
2022-01-15 19:17:51 +0800

    Some JavaDocs for main interfaces

--- src/main/java/com/kensbunker/gdel/enums/CostRule.java
+++ src/main/java/com/kensbunker/gdel/enums/CostRule.java
@@ -1,19 +1,25 @@
 package com.kensbunker.gdel.enums;
 
+import com.kensbunker.gdel.request.GParcel;
 import com.kensbunker.gdel.rules.*;
 import lombok.Getter;
 
+/**
+ * Defines the different rules for Cost Computation of a {@link GParcel} Since we are using an enum,
+ * ordinal should be preserved and defines the precedence on which rules should be evaluated first.
+ */
 @Getter
 public enum CostRule {
-    REJECT("Reject", new RejectConditionRule()),
-    HEAVY_PARCEL("Heavy Parcel", new HeavyParcelConditionRule()),
-    SMALL_PARCEL("Small Parcel", new SmallParcelConditionRule()),
-    MEDIUM_PARCEL("Medium Parcel", new MediumParcelConditionRule()),
-    LARGE_PARCEL("Large Parcel", new LargeParcelConditionRule());
-    private String label;
-    private ConditionRule conditionRule;
-    CostRule(String label, ConditionRule conditionRule) {
-        this.label = label;
-        this.conditionRule = conditionRule;
-    }
+  REJECT("Reject", new RejectConditionRule()),
+  HEAVY_PARCEL("Heavy Parcel", new HeavyParcelConditionRule()),
+  SMALL_PARCEL("Small Parcel", new SmallParcelConditionRule()),
+  MEDIUM_PARCEL("Medium Parcel", new MediumParcelConditionRule()),
+  LARGE_PARCEL("Large Parcel", new LargeParcelConditionRule());
+  private String label;
+  private ConditionRule conditionRule;
+
+  CostRule(String label, ConditionRule conditionRule) {
+    this.label = label;
+    this.conditionRule = conditionRule;
+  }
 }
--- src/main/java/com/kensbunker/gdel/rules/ConditionRule.java
+++ src/main/java/com/kensbunker/gdel/rules/ConditionRule.java
@@ -2,7 +2,25 @@ package com.kensbunker.gdel.rules;
 
 import com.kensbunker.gdel.request.GParcel;
 
+/**
+ * This defines the contract for creating a condition rule. A {@link ConditionRule} instance is used
+ * in a particular {@link com.kensbunker.gdel.enums.CostRule} which defines the condition to run
+ * against the {@link GParcel} instance to check if it should use that {@link
+ * com.kensbunker.gdel.enums.CostRule} for computing the cost.
+ */
 public interface ConditionRule {
+    /**
+     * Call this method to check if {@link GParcel} passes the condition
+     * @param parcel the {@link GParcel} to check for this condition
+     * @return true if it passes the condition, false otherwise
+     */
     boolean pass(GParcel parcel);
+
+    /**
+     * Defines the computation of the cost. For example,
+     * multiply 10.0 by the weight of the {@link GParcel}
+     * @param parcel, the {@link GParcel} instance
+     * @return the computed cost
+     */
     double computeCost(GParcel parcel);
 }
